6ea1ecb7a517506ed74a45d97dec521a41f59d6f
Dmytro Martynenko <dmytromartynenko1@gmail.com>
2023-02-14 22:19:47 +0000

    make the selected buttons change colors

--- assets/java/script.js
+++ assets/java/script.js
@@ -15,3 +15,22 @@ for (i = 1; i <= 12; i++){
     button.textContent= i;
     twelveButtons.appendChild(button);
 }
+
+/*Make Buttons change colors when clicked*/
+
+let color= fiftyButtons.children;
+let colorTwo= twelveButtons.children;
+
+function boxClicked(){
+if (this.style.backgroundColor === "yellow") {
+        this.style.backgroundColor = "white";
+    } else {
+        this.style.backgroundColor = "yellow";
+    }
+}
+for (let i = 0; i < color.length; i++) {
+    color[i].addEventListener('click', boxClicked);
+}
+for (let i = 0; i < colorTwo.length; i++) {
+    colorTwo[i].addEventListener('click', boxClicked);
+}
