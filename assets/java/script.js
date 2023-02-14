1671490342c0fd397bfd3a0b7e1f78244a9ba1b9
Dmytro Martynenko <dmytromartynenko1@gmail.com>
2023-02-14 17:59:21 +0000

    Add the submit button and syle it

--- assets/java/script.js
+++ assets/java/script.js
@@ -7,3 +7,11 @@ for (i = 1; i <= 50; i++){
     fiftyButtons.appendChild(button);
 }
 
+/*Create 12 buttons for html*/
+let twelveButtons= document.getElementById("twelve-buttons");
+
+for (i = 1; i <= 12; i++){
+    let button= document.createElement("button");
+    button.textContent= i;
+    twelveButtons.appendChild(button);
+}
