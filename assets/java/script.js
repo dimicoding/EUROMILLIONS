ea7ed963cf905cac4cd8018b63a965007dda8885
Dmytro Martynenko <dmytromartynenko1@gmail.com>
2023-02-16 16:20:05 +0000

    cleaning up the code after gitpod malfunction, add clicked buttons to be displayed in the inputs without repetition. add background color when button is pressed

--- assets/java/script.js
+++ assets/java/script.js
@@ -5,136 +5,58 @@ for (let i = 1; i <= 50; i++) {
     let button = document.createElement("button");
     button.innerText = i;
     fiftyButtons.appendChild(button);
-
-    let clicks = 0;
-    button.addEventListener("click", function () {
-        clicks++;
-
-        if (clicks >= 5) {
-            button.disabled = true;
-        }
-    });
 }
 
 /**display 5 buttons into 5input boxes
- *
- * 
+ *add event listeners
+ * dont allow the same button to repeat
  */
 let selectedButtons = document.querySelectorAll('#fifty-buttons button');
 let inputBox = document.querySelectorAll('input[type="text"]');
 let maxActiveButtons = 5;
+let selectedValues= [];
 
 selectedButtons.forEach(button => {
     button.addEventListener('click', () => {
-
         let activeButtons = document.querySelectorAll('#fifty-buttons button.active');
         if (activeButtons.length === maxActiveButtons) {
             return;
         }
+        if (selectedValues.includes(button.textContent)){
+            return;
+        }
         button.classList.add('active');
+        button.style.backgroundColor= "yellow";
         for (let i = 0; i < inputBox.length; i++) {
             if (inputBox[i].value === '') {
                 inputBox[i].value = button.textContent;
+                selectedValues.push(button.textContent);
                 break;
-            }
+            }   
         }
-
     });
 });
+
+
 /**
  * generate random numbers
  * without repeating on different input boxes
  */
-let correctAnswer= document.querySelectorAll("#correct-answer input");
-let randomNumbers= [];
+let correctAnswer = document.querySelectorAll("#correct-answer input");
+let randomNumbers = [];
 
-while (randomNumbers.length < 5){
-    let randomNumber= Math.floor(Math.random() *50) +1;
-    if (!randomNumbers.includes(randomNumber)){
+while (randomNumbers.length < 5) {
+    let randomNumber = Math.floor(Math.random() * 50) + 1;
+    if (!randomNumbers.includes(randomNumber)) {
         randomNumbers.push(randomNumber);
     }
-} 
+}
 for (let i = 0; i < correctAnswer.length; i++) {
     correctAnswer[i].value = randomNumbers[i];
-  }
+}
 
-
-
-
-
-
-
-
-
-
-/** 
-for (i = 1; i <= 50; i++) {
-    let button = document.createElement("button");
-    button.textContent = i;
-    let clicks = 0;
-
-    button.addEventListener('click', function () {
-        clicks++;
-
-        if (clicks >= 5) {
-            button.disabled = true;
-        }
-    });
-    fiftyButtons.appendChild(button);
-}*/
-/**
- * limit 50buttons to a max of 5clicks
- * 
- * let pickFiveFifty= document.querySelectorAll("#fifty-buttons button");
-pickFiveFifty.forEach((button) => {
-    let clicks= 0;
-    button.addEventListener("click", function(){
-        clicks++;
-        if(clicks >=5){
-            pickFiveFifty.disabled= true;
-        }
-    });
-});
- * 
+/**Create Cheack answer function
+ * compare random number value with isers-input
+ * make the function work afre the user clicks on the button
  */
 
-
-
-
-
-
-
-/*Create 12 buttons for html*/
-let twelveButtons = document.getElementById("twelve-buttons");
-
-for (i = 1; i <= 12; i++) {
-    let button = document.createElement("button");
-    button.textContent = i;
-    twelveButtons.appendChild(button);
-};
-
-/*Make Buttons change colors when clicked*/
-
-let color = fiftyButtons.children;
-let colorTwo = twelveButtons.children;
-
-function boxClicked() {
-    if (this.style.backgroundColor === "yellow") {
-        this.style.backgroundColor = "white";
-    } else {
-        this.style.backgroundColor = "yellow";
-    }
-}
-for (let i = 0; i < color.length; i++) {
-    color[i].addEventListener('click', boxClicked);
-    let = clicks = 0;
-    if (clicks >= 5) {
-        color.disabled = true;
-    }
-
-
-
-}
-for (let i = 0; i < colorTwo.length; i++) {
-    colorTwo[i].addEventListener('click', boxClicked);
-}
