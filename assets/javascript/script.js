dc00ca7e1fa4993b68efb74b5a220270b9e68a99
Dmytro Martynenko <dmytromartynenko1@gmail.com>
2023-06-13 13:27:41 +0000

    Add allFilled and allValid variables

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,4 @@
-alert("Welcome to Euromillions!\nTo test your odds is very simple:\n *   Pick your 5 lucky numbers from 1-50;\n *   'Click' on the submit button;\n *   'Click' on retry to start over;\nDon't give up! You're almost a millionaire...");
-
-
-/*Create 50 buttons for html*/
+/* Create 50 buttons for html */
 let fiftyButtons = document.getElementById("fifty-buttons");
 
 for (let i = 1; i <= 50; i++) {
@@ -10,9 +7,9 @@ for (let i = 1; i <= 50; i++) {
     fiftyButtons.appendChild(button);
 }
 
-/**display 5 buttons into 5input boxes
- *add event listeners
- * dont allow the same button to repeat
+/** Display 5 buttons into 5 input boxes
+ * Add event listeners
+ * Don't allow the same button to repeat
  */
 let selectedButtons = document.querySelectorAll('#fifty-buttons button');
 let inputBox = document.getElementById('user-input').children;
@@ -42,92 +39,121 @@ selectedButtons.forEach(button => {
 
 
 /**
- * generate random numbers
- * without repeating on different input boxes
+ * Generate random numbers
+ * Without repeating on different input boxes
  */
 let correctAnswer = document.querySelectorAll("#correct-answer input");
 let submitButton = document.getElementById("submit-button");
 
 submitButton.addEventListener('click', function () {
-    for (let i = 0; i < correctAnswer.length; i++) {
-        correctAnswer[i].value = "";
-    }
+    let userAnswer = document.querySelectorAll('#user-input input');
+    let allFilled = true;
 
-    let randomNumbers = [];
-    while (randomNumbers.length < 5) {
-        let randomNumber = Math.floor(Math.random() * 50) + 1;
-        if (!randomNumbers.includes(randomNumber)) {
-            randomNumbers.push(randomNumber);
+    for (let i = 0; i < userAnswer.length; i++) {
+        if (userAnswer[i].value === '') {
+            allFilled = false;
+            alert("Please select a number.");
+            break;
         }
     }
-    for (let i = 0; i < correctAnswer.length; i++) {
-        correctAnswer[i].value = randomNumbers[i];
+
+    if (allFilled) {
+        showCorrectAnswer();
+
+        // Rest of the code to generate random numbers
+        for (let i = 0; i < correctAnswer.length; i++) {
+            correctAnswer[i].value = "";
+        }
+
+        let randomNumbers = [];
+        while (randomNumbers.length < 5) {
+            let randomNumber = Math.floor(Math.random() * 50) + 1;
+            if (!randomNumbers.includes(randomNumber)) {
+                randomNumbers.push(randomNumber);
+            }
+        }
+        for (let i = 0; i < correctAnswer.length; i++) {
+            correctAnswer[i].value = randomNumbers[i];
+        }
     }
 });
 
 
-/**Create Cheack answer function
- * compare random number value with isers-input
- * make the function work after the user clicks on the button
+/** Create Check answer function
+ * Compare random number value with user's input
+ * Make the function work after the user clicks on the button
  */
 function showCorrectAnswer() {
     let userAnswer = document.querySelectorAll('#user-input input');
+    let allValid = true;
 
     let correct = [];
     for (let i = 0; i < correctAnswer.length; i++) {
         correct.push(parseInt(correctAnswer[i].value));
-
     }
 
     let outcome = "";
     for (let i = 0; i < userAnswer.length; i++) {
         let selectedNumber = parseInt(userAnswer[i].value);
 
+        if (isNaN(selectedNumber)) {
+            allValid = false;
+            outcome += '<input style="background-color: indianred" value="">';
+            alert("Please enter numbers in all boxes.");
+            continue;
+        }
+
         if (selectedNumber === correct[i]) {
             outcome += '<input style="background-color: mediumseagreen" value="' + selectedNumber + '">';
-            alert(`Congratulations! You've got the number ${selectedNumber} rigth!` );
+            alert(`Congratulations! You've got the number ${selectedNumber} right!`);
         } else {
             outcome += '<input style="background-color: indianred" value="' + selectedNumber + '">';
-        } 
+        }
     }
+
+    // Clear user input if NaN encountered
+    if (!allValid) {
+        for (let i = 0; i < userAnswer.length; i++) {
+            userAnswer[i].value = "";
+        }
+        document.getElementById("user-input").innerHTML = outcome;
+        return;
+    }
+
     document.getElementById("user-input").innerHTML = outcome;
 
     let userInputElement = document.getElementById("user-input");
     userInputElement.innerHTML = '';
-    
-    
+
     userInputElement.innerHTML += outcome;
+
+    // Clear correct answer boxes after submit button is clicked
+    for (let i = 0; i < correctAnswer.length; i++) {
+        correctAnswer[i].value = "";
+    }
 }
-submitButton.addEventListener('click', showCorrectAnswer);
 
-
-let reset= document.getElementById("reset-button");
-reset.addEventListener('click', function() {
-
-
+let reset = document.getElementById("reset-button");
+reset.addEventListener('click', function () {
     selectedButtons.forEach(button => {
         button.classList.remove("active");
         button.style.backgroundColor = "";
     });
-    
+
     for (let i = 0; i < inputBox.length; i++) {
         inputBox[i].value = "";
         inputBox[i].style.backgroundColor = "";
-        inputBox[i].disabled= false;
+        inputBox[i].disabled = false;
     }
-    
 
     selectedValues = [];
     for (let i = 0; i < correctAnswer.length; i++) {
         correctAnswer[i].value = "";
-        
     }
- 
 });
 
 
-
-/*Create a countdown reverse clock "jackpot" updating every week */
+/* Create a countdown reverse clock "jackpot" updating every week */
 let days = document.getElementById("day");
 let hours = document.getElementById("hour");
 let minutes = document.getElementById("minute");
